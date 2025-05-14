5fbaef0844588004a071ade97a79186da113988f
Moiz Ali <moizmughalali@gmail.com>
2025-05-14 20:41:13 +0500

    just completed first module of Phase III.

--- server/utils/pisihing/detectFakeTokenContract.js
+++ server/utils/pisihing/detectFakeTokenContract.js
@@ -6,6 +6,8 @@ const EtherScanURL = process.env.ETHERSCAN_API_ENDPOINT;
 const EtherScanAPIKEY = process.env.ETHERSCAN_API_KEY;
 
 const fakeTokenDetection = async (_tokenAddress) => {
+  
+  // getting coin list from etherscan
   const tokenInfoAPI = `${EtherScanURL}?module=token&action=tokeninfo&contractaddress=${_tokenAddress}&apikey=${EtherScanAPIKEY}`;
 
   const response = await axios.get(tokenInfoAPI);
@@ -19,5 +21,35 @@ const fakeTokenDetection = async (_tokenAddress) => {
     totalSupply: Number(data.totalSupply)
   }
 
+  // getting coin list from coin gecko
+  const coinList = axios.get(process.env.COIN_GECKO_NAME_SYMBOL_API);
+
+  // fetching coin_id and comparing both symbols
+  const coinId = coinList.data.find(coin => coin.symbol.toLowerCase() === tokenInfo.symbol.toLowerCase())?.id;
+
+  // fetching full token data
+  const sepecificCoinId = `${process.env.COIN_GECKO_SEPECIFIC_ID.replace('{id}', coinId)}`;
+  const tokenDataRes = await axios.get(sepecificCoinId);
+  const tokenData = tokenDataRes.data;
+
+  if (
+    tokenInfo.name === tokenData.name &&
+    tokenInfo.symbol === tokenData.symbol &&
+    tokenInfo.totalSupply === tokenData.market_data?.totalSupply
+  ){
+    return {
+      success: true,
+      risk: false,
+      message: `The token associated to this token address ${_tokenAddress} is valid` 
+    }
+  }else{
+    return{
+      success: true,
+      risk: true,
+      message: `The token associated to this token address ${_tokenAddress} is Fake`
+    }
+  }
 
 };
+
+module.exports = fakeTokenDetection;
