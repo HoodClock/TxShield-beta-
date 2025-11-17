f8622e63575e964f0191a04a620dcff354777f44
Moiz Ali <moizmughalali@gmail.com>
2025-11-17 09:37:08 +0500

    Resolve some more conflicts

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React from 'react'
 import HomeClient from './HomeClient'
-=======
-import Home from "./home/page"
->>>>>>> d5e225ca75a92ffe1a6b54a165cbd61e9bccab29
 
 export default function Page() {
   return (
