6fd03ad13d636146860f3f951034073de35ec78c
NeilDaNail <huzaifakhan.9139@gmail.com>
2025-11-23 00:35:21 +0500

    added a scroll fill-up bar

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "./clientLayout";
+import ScrollProgressBar from "./components/ScrollProgressBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} dark`}>
       <body className="antialiased bg-background text-foreground">
         <ClientLayout>
+          <ScrollProgressBar />
           {children}
         </ClientLayout>
       </body>
