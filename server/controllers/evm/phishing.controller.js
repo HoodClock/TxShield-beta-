aedacd3cd288ee27bb652467c52fa10024090325
Moiz Ali <moizmughalali@gmail.com>
2026-03-29 20:29:55 +0500

    Just make Phishing response

--- server/controllers/evm/phishing.controller.js
+++ server/controllers/evm/phishing.controller.js
@@ -2,20 +2,13 @@ const {
   evmPhishingValidator,
 } = require("../../validators/evm/evmPhishing.validator");
 const {
-  calculatePhishingRisks,
-} = require("../../utils/calculatePhishingRisks.utils");
-const {
-  detectApprovalScam,
-  detectEtherForwarding,
-  detectMaliciousProxy,
-  detectPermitPhishing,
-  detectPhishingDomainLink,
-} = require("../../services/evm/phishing/index");
+  detectPhishing,
+} = require("../../services/evm/phishing/phishingService");
 const { getAddress } = require("ethers");
 
 const MasterPhishingController = async (req, res) => {
   try {
-    const { userAddress, recepientAddress, currencySymbol, chainId } = req.body;
+    const { recepientAddress, chainId } = req.body;
 
     // validation
     const validation = evmPhishingValidator(req.body);
@@ -24,47 +17,13 @@ const MasterPhishingController = async (req, res) => {
         .status(400)
         .json({ success: false, message: validation.message });
 
-    const normalUserAddress = getAddress(userAddress.toLowerCase());
     const normalContractAddress = getAddress(recepientAddress.toLowerCase());
 
-    const [approveScam, etherForwarding, proxyScam, permitCheck, domainCheck] =
-      await Promise.all([
-        detectApprovalScam(
-          normalUserAddress,
-          normalContractAddress,
-          currencySymbol,
-          chainId,
-        ),
-        detectEtherForwarding(normalContractAddress, currencySymbol, chainId),
-        detectMaliciousProxy(normalContractAddress, currencySymbol, chainId),
-        detectPermitPhishing(normalContractAddress, currencySymbol, chainId),
-        detectPhishingDomainLink(
-          normalContractAddress,
-          currencySymbol,
-          chainId,
-        ),
-      ]);
-
-    const results = [
-      { name: "approvalScam", data: approveScam },
-      { name: "etherForwarding", data: etherForwarding },
-      { name: "proxyScam", data: proxyScam },
-      { name: "permitCheck", data: permitCheck },
-      { name: "domainCheck", data: domainCheck },
-    ];
-
-    const summery = calculatePhishingRisks(results);
+    const result = await detectPhishing(normalContractAddress, chainId);
 
     return res.status(200).json({
       success: true,
-      riskSummery: summery,
-      details: {
-        approveScam,
-        etherForwarding,
-        proxyScam,
-        permitCheck,
-        domainCheck,
-      },
+      data: result,
     });
   } catch (err) {
     console.error("Phishing check error:", err.message);
