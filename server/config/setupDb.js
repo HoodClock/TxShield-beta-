e5f97ea2d26b184b0ccb7e55f85f53d24644c817
Moiz Ali <moizmughalali@gmail.com>
2026-03-30 12:32:55 +0500

    Just added a Bull-MQ queue for job handling more then one request at one time & corrected the payload and implement the redis-cache on simulation + honeypot + phishing

--- server/config/setupDb.js
+++ server/config/setupDb.js
@@ -3,7 +3,7 @@ const pool = require("./db");
 /**
  * for devs:
  * @async
- * @function setup
+ * @function -> "your function name here"
  * @description Initialize api_keys table: {id, api_key, wallet, tier, request_count, created_at, last_used}
  * @returns {Promise<void>} -> using pool here from ./db.js {@example -> pool.query("your query")} with await returns void
  * @todo call a funciton simply at the end -> {setup}
