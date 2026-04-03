a3df0144f8f4bad3307103fd30cbddf69a98f0a1
CoffeeTheBest <huzaifakhan.9139@gmail.com>
2026-04-03 20:04:35 +0500

    configured a db and switched wagmi to thirdweb

--- client/src/lib/wagmi.js
+++ client/src/lib/wagmi.js
@@ -1,18 +1,2 @@
-"use client";
-
-import { http } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-
-export const chains = [mainnet, sepolia];
-
-export const wagmiConfig = getDefaultConfig({
-  appName: "TxShield",
-  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-  ssr: true,
-});
+// wagmi has been uninstalled in favor of thirdweb
+export {};
